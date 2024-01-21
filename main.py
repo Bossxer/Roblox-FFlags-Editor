3b0af159ef1a4c233678e0c122856cc01718c249
bossxerr <70553957+Bossxer@users.noreply.github.com>
2024-01-21 00:27:30 -0600

    Update main.py
    Small lil fix to make the folder if doesnt exist and skips if it does

--- main.py
+++ main.py
@@ -14,5 +14,12 @@ function.CTitle("FFlag Editor - Stable")
 function.setup()
 selected_fflags = []
 
+try:
+    with open(pathlib.Path(os.getenv('LOCALAPPDATA')) / 'Roblox/Versions/' / function.find_latest_modified_folder(pathlib.Path(os.getenv('LOCALAPPDATA')) / 'Roblox/Versions') / 'ClientSettings/ClientAppSettings.json', "r") as outfile:
+        print("Opened ClientSettings folder")
+except:
+    print(colored("Failed to open ClientSettings folder, creating it.", "red"))
+    pathlib.Path(pathlib.Path(os.getenv('LOCALAPPDATA')) / 'Roblox/Versions/' / function.find_latest_modified_folder(pathlib.Path(os.getenv('LOCALAPPDATA')) / 'Roblox/Versions') / 'ClientSettings').mkdir(parents=True, exist_ok=False)
+
 if __name__ == "__main__":
     function.main(selected_fflags)
